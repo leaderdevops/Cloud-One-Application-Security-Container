b4463deb0f9ababf6045aebbc62401a142b86436
Will Robinson <will@oznetnerd.com>
2019-08-26 16:20:50 +1000

    Made changes so code works on both Mac & Linux. Also updated docs and removed key file.

--- IMMUNIO/docker-build.sh
+++ IMMUNIO/docker-build.sh
@@ -9,10 +9,6 @@ mkdir -p ../build
 cp ../upstream/taskManager/wsgi.py ../build/wsgi.py
 cp ../upstream/runapp.sh ../build/runapp.sh
 
-# `manage.py runserver` in python3 binds to all IPs, in python2 we need to
-# explicitly request it. Add the bind IP:PORT to the command
-sed -i '' '${s/$/ 0.0.0.0:8000/;}' ../build/runapp.sh
-
 # Build one image without App Protect running
 docker build -t djangonv  ../
 
